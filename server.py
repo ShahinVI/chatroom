0847d7a2f38db86162744d863b2af2ab13cc06f7
Aiman Shahin <shahin@MatchXs-MacBook-Air.local>
2023-04-15 02:03:52 +0200

    update server 1

--- server.py
+++ server.py
@@ -1,24 +1,39 @@
 import socket
+#################################################
+host = '127.0.0.1'                              #
+port = 8080                                     #
+message = "Hey connection established"          #
+# dict of clients holding connection ############
+clients = {}                                    #
+# dict of address holding address of clients ####
+addresses = {}                                    #
+#################################################
 
-host='localhost'
-port=8080
+def establish_connection():
+    # TCP connecttion use SOCK_STREAM. SOCK_DGRAM using UDP connection
+    # AF_INET uses internet 4
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # passing the host and port number as a tuple
+    sock.bind((host, port))
+    return sock
 
-# TCP connecttion use SOCK_STREAM. SOCK_DGRAM using UDP connection
-# AF_INET uses internet 4
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# passing the host and port number as a tuple
-sock.bind((host,port))
+def listen_accept_connection(sock):
+    while True:
+        # listening 5 request at one time for simplicity
+        sock.listen(5)
+        print("server is running and listening to clients request\n")
+        # when connection establish use sock.accept
+        # this gives back connection and address
+        conn, address = sock.accept()
+        if conn:
+            return conn, address
 
-# listening 1 request at one time
-sock.listen(1)
+def send_message(sock,message):
+    # send message over the coonnection
+    conn.send(message.encode())
 
-# when connection establish use sock.accept
-# this gives back connection and address
-conn,address = sock.accept()
-
-message = "Hey connection established"
-
-# send message over the coonnection
-conn.send(message.encode())
-
-conn.close()
+if __name__=="__main__":
+    sock = establish_connection()
+    conn, address = listen_accept_connection(sock)
+    send_message(sock, message)
+    conn.close()
